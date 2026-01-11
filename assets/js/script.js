f88b233f126a829d5758dbb2d6653217735a46fc
Alicia <alicia.alexia03@gmail.com>
2026-01-11 15:55:30 -0300

    feat: add floating numeric indicator to display read percentage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 const progressBar = document.getElementById('progress-bar');
-function updateProgressBar() {
+const progressText = document.getElementById('progress-text'); 
+
+const updateProgressBar = () => {
     const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
     const clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
-    const scrolledPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
-    progressBar.style.width = scrolledPercentage + "%";
+
+    const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    const scrolledRounded = Math.round(scrolled);
+    progressBar.style.width = `${scrolled}%`;
+    progressText.innerText = `${scrolledRounded}%`;
+    progressText.style.left = `${scrolled}%`;
+    
+    if (scrolled > 1) {
+        progressText.classList.remove('opacity-0');
+    } else {
+        progressText.classList.add('opacity-0');
+    }
 }
 window.addEventListener('scroll', updateProgressBar);
